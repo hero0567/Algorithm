5fc66cf6ceb5405d107e3bc8e4613881e74687d2
levy.xia <hero0567@163.com>
2019-04-04 21:04:09 +0800

    string split pass

--- src/main/java/com/levy/algorithm/backtracking/StringSplitGroup.java
+++ src/main/java/com/levy/algorithm/backtracking/StringSplitGroup.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 把字符串分解我不同的组
+ */
 public class StringSplitGroup {
 
     @Test
@@ -12,8 +15,6 @@ public class StringSplitGroup {
         String s = "hello";
         List<String> result = new ArrayList<>();
         splitStringToGroup(s, "", result);
-
-
         for (String r : result) {
             System.out.println(r);
         }
@@ -28,16 +29,14 @@ public class StringSplitGroup {
 
     public void splitStringToGroup(String s, String parent, List<String> result) {
         if (s.length() == 1) {
-            result.add(parent + s);
             return;
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            String head = parent + "(" + s.substring(0, 0 + 1) + ")";
-            String tail = "(" + s.substring(0 + 1, s.length()) + ")";
-            result.add(head + tail);
-            splitStringToGroup(tail, parent, result);
-
+        for (int i = 0; i < s.length() - 1; i++) {
+            String head = parent + "(" + s.substring(0, i + 1) + ")";
+            String tail = s.substring(i + 1, s.length());
+            result.add(head + "(" + tail + ")");
+            splitStringToGroup(tail, head, result);
         }
     }
 }
